14fb470b8349f54536f25c6cfeac0437fdc121e8
goarnaiz <72554806+goarnaiz@users.noreply.github.com>
2023-01-06 02:27:15 +0100

    Update Encuesta.java

--- Trabajo/src/Encuesta.java
+++ Trabajo/src/Encuesta.java
@@ -1,25 +1,26 @@
 import java.util.ArrayList;
 
 public class Encuesta {
-	
+
 	private String tema;
 	private ArrayList<Pregunta> preguntas;
-	
+
 	public Encuesta(String tema, ArrayList<Pregunta> preguntas) {
+		this.tema = tema;
 		this.preguntas = preguntas;
 	}
-
 
 	public void anadirPregunta(Pregunta p) {
 		this.preguntas.add(p);
 	}
-	
+
 	public String getTema() {
 		return this.tema;
 	}
-	
+
 	public ArrayList<Pregunta> getPreguntas() {
 		return this.preguntas;
 	}
 
 }
+
