7b471f59c4312e238d63366ed20e5c63090362a8
goarnaiz <72554806+goarnaiz@users.noreply.github.com>
2023-01-07 21:20:07 +0100

    Update Servidor.java

--- Trabajo/src/Servidor.java
+++ Trabajo/src/Servidor.java
@@ -1,9 +1,13 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -12,12 +16,12 @@ public class Servidor {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		ExecutorService pool = Executors.newCachedThreadPool();
+		HashMap<String, String> listaVotos = new HashMap<String, String>();
+
 		try (ServerSocket ss = new ServerSocket(6666)) {
 
-			ExecutorService pool = Executors.newCachedThreadPool();
-
 			// Cargo las encuestas de mi base de datos virtualizada
-			// Preguntas encuesta política
 
 			Encuesta politica = new Encuesta("politica", new ArrayList<Pregunta>());
 			Encuesta economia = new Encuesta("economia", new ArrayList<Pregunta>());
@@ -31,21 +35,33 @@ public class Servidor {
 				try {
 					Socket s;
 					s = ss.accept();
-					DataOutputStream dos = new DataOutputStream(s.getOutputStream());
 					DataInputStream dis = new DataInputStream(s.getInputStream());
 
-					System.out.println("Hola cliente " + s.getInetAddress().getHostName());
+					String hostName = s.getInetAddress().getHostName();
 
-					int numSeleccionado = Integer.parseInt(dis.readLine());
+					System.out.println("Hola cliente " + hostName);
 
-					// System.out.println(s.getPort());
+					// compruebo que el host no ha realizado ningún voto, a excepción de localhost
+					// que puede realizar los votos que quiera (para realizar pruebas)
+					if (!listaVotos.containsValue(hostName) || hostName.equals("127.0.0.1")) {
+						SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+						String fecha = sdf.format(new Date());
+						listaVotos.put(fecha, s.getInetAddress().getHostName());
 
-					AtenderPeticion ap = new AtenderPeticion(numSeleccionado, bdEncuesta, s);
-					pool.execute(ap);
+						int numSeleccionado = Integer.parseInt(dis.readLine());
 
-					// pool.shutdown();
+						System.out.println("Lista de usuarios que han votado");
+						for (String clave : listaVotos.keySet()) {
+							System.out.println("Fecha " + clave + "\tHost: " + listaVotos.get(clave));
+						}
 
-					// s.close();
+						AtenderPeticion ap = new AtenderPeticion(numSeleccionado, bdEncuesta, s);
+						pool.execute(ap);
+
+					} else {
+						System.out.println("Ya se ha realizado un voto desde este host");
+						// s.close();
+					}
 
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
@@ -56,9 +72,10 @@ public class Servidor {
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
+		} finally {
+			pool.shutdown();
 		}
 
 	}
 
 }
-
